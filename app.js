5313bedd8735eb295c6c8220156880ec84de7c91
Allyn777 <allynmarcdumapias202300160@gmail.com>
2024-10-02 22:03:10 -0400

    finish

--- app.js
+++ app.js
@@ -1,13 +1,11 @@
-  // Initialize EmailJS with your user ID
   (function(){
     emailjs.init('FHm4R-D_YdrBidvcN');
 })();
 
 
-// Function to send email using EmailJS
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
-    // Send the email using the form data
+    
     console.log(this)
     emailjs.sendForm('service_0m94wca', 'template_nw0upln', this)
         .then(function() {
